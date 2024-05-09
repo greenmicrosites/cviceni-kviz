960754f22730eaff7eeb67037902277f0796567f
Petra Vicherkova <peta.vicherkova@gmail.com>
2024-05-09 16:47:54 +0200

    kviz

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+const question = document.querySelector(".question")
+question.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon."
+
+const answerElement = document.querySelector(".answer__text")
+const answer = prompt(question.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.")
+
+answerElement.textContent = answer
+
+if (answer == "Bell") {
+    answerElement.classList.add("answer--correct") 
+} else {
+    answerElement.classList.add("answer--wrong")
+}
+
